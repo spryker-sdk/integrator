d78d2629833969836c596de6e6ba4d3e5d068eb6
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2021-08-31 16:51:19 +0300

    TE-9775 Finished clean up and integrate phpunit

---
+++ tests/SprykerSdkTest/Zed/Integrator/Business/TempTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SprykerSdkTest\Zed\Integrator\Business;
+
+
+use SprykerSdkTest\Zed\Integrator\BaseTestCase;
+
+class TempTest extends BaseTestCase
+{
+    public function testSimple()
+    {
+        $this->assertEquals(true, true);
+    }
+}
