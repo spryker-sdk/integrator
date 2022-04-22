6b313960be66b2b544b21a9410083dd1fb1d0577
Sento2012 <zmei123@yandex.ru>
2022-04-22 15:47:22 +0300

    PAAS-1042 environment configuration logic (#25)
    * PAAS-1042 environment configuration logic
    
    * PAAS-1042 environment configuration logic
    
    * PAAS-1042 code review fixes
    
    * PAAS-1042 code review fixes
    
    * PAAS-1180 fix additional configuration environment cases
    
    * PAAS-1180 fix additional configuration environment cases
    
    * PAAS-1180 code style fixes
    
    * PAAS-1180 code review fixes
    
    * PAAS-1180 code review fixes
    
    * PAAS-1042 boolean expression support
    
    Co-authored-by: Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>

---
+++ src/Builder/ConfigurationEnvironmentBuilder/ArrayConfigurationEnvironmentStrategy.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace SprykerSdk\Integrator\Builder\ConfigurationEnvironmentBuilder;
+
+use SprykerSdk\Integrator\IntegratorConfig;
+
+class ArrayConfigurationEnvironmentStrategy implements ConfigurationEnvironmentStrategyInterface
+{
+    /**
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function isApplicable($value): bool
+    {
+        if (!is_array($value) || !empty($value[IntegratorConfig::MANIFEST_KEY_IS_LITERAL])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return string
+     */
+    public function getFormattedExpression($value): string
+    {
+        return $this->createExpressionStringFromValue($value);
+    }
+
+    /**
+     * @param mixed $value
+     * @param int $level
+     *
+     * @return string
+     */
+    protected function createExpressionStringFromValue($value, int $level = 0): string
+    {
+        $level++;
+        $result = ['['];
+        foreach ($value as $keyValueItem => $valueItem) {
+            $outputFormat = '\'%s\',';
+            if (is_array($valueItem)) {
+                $valueItem = $this->createExpressionStringFromValue($valueItem, $level);
+                $outputFormat = '%s,';
+            }
+            if (is_int($keyValueItem)) {
+                $result[] = $this->createIndent($level) . sprintf($outputFormat, trim($valueItem, '\''));
+
+                continue;
+            }
+            $formattedKeyValue = $this->getFormattedValueExpression($keyValueItem);
+            $formattedValue = $this->getFormattedValueExpression($valueItem);
+            $result[] = $this->createIndent($level) . sprintf('%s => %s,', $formattedKeyValue, $formattedValue);
+        }
+        $result[] = $this->createIndent($level - 1) . ']';
+
+        return implode("\n", $result);
+    }
+
+    /**
+     * @param int $level
+     *
+     * @return string
+     */
+    protected function createIndent(int $level): string
+    {
+        return str_repeat(' ', ($level) * 4);
+    }
+
+    /**
+     * @param string $expression
+     *
+     * @return bool
+     */
+    protected function isComplicatedExpression(string $expression): bool
+    {
+        return (bool)preg_match('/[\(\)\?]/', $expression);
+    }
+
+    /**
+     * @param string $expression
+     *
+     * @return string
+     */
+    protected function getFormattedValueExpression(string $expression): string
+    {
+        if ($this->isComplicatedExpression($expression)) {
+            return $expression;
+        }
+
+        return sprintf('\'%s\'', trim($expression, '\''));
+    }
+}
