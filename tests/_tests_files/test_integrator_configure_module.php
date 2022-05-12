971fc12df7f04651f0a55f67d4e5649cce065def
Bohdan Turchyk <bohdan.turchyk@spryker.com>
2022-05-12 13:24:10 +0300

    PPLUS-1265 Fixed  manifest type behavior to skip empty values

--- tests/_tests_files/test_integrator_configure_module.php
+++ tests/_tests_files/test_integrator_configure_module.php
@@ -31,6 +31,14 @@ class TestIntegratorDefaultConfig extends BaseConfig
     }
 
     /**
+     * @return string
+     */
+    public function testChangeMissingValue(): string
+    {
+        return 'test';
+    }
+
+    /**
      * @return array
      */
     public function testChange2(): array
