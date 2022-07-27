83d946583a2b1cf7110cc466c246869bd43c6466
v.strelchenko <v.strelchenko@gamingtec.com>
2022-07-27 15:52:01 +0300

    PPLUS-1142: Console commands integrator logic / review fixes

--- src/ModuleFinder/Module/ModuleFinder/ModuleFinder.php
+++ src/ModuleFinder/Module/ModuleFinder/ModuleFinder.php
@@ -101,7 +101,7 @@ class ModuleFinder implements ModuleFinderInterface
     }
 
     /**
-     * @return \Symfony\Component\Finder\Finder|\Symfony\Component\Finder\SplFileInfo[]
+     * @return \Symfony\Component\Finder\Finder<\Symfony\Component\Finder\SplFileInfo>
      */
     protected function getStandaloneModuleFinder(): Finder
     {
@@ -178,7 +178,7 @@ class ModuleFinder implements ModuleFinderInterface
     }
 
     /**
-     * @return \Symfony\Component\Finder\Finder|\Symfony\Component\Finder\SplFileInfo[]
+     * @return \Symfony\Component\Finder\Finder<\Symfony\Component\Finder\SplFileInfo>
      */
     protected function getModuleFinder(): Finder
     {
