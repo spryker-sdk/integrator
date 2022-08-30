966fa3dcc5cea6090dad3d3a1724a9a4b26a6d64
Dmytro Klyman <dmytro.klyman@spryker.com>
2022-08-30 15:42:30 +0200

    Improve code coverage by adding new tests

---
+++ tests/SprykerSdkTest/Integrator/Dependency/Console/ComposerInputOutputAdapterTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdkTest\Integrator\Dependency\Console;
+
+use Composer\IO\NullIO;
+use PHPUnit\Framework\TestCase;
+use SprykerSdk\Integrator\Dependency\Console\ComposerInputOutputAdapter;
+
+class ComposerInputOutputAdapterTest extends TestCase
+{
+    /**
+     * @var \SprykerSdk\Integrator\Dependency\Console\ComposerInputOutputAdapter
+     */
+    protected ComposerInputOutputAdapter $adapter;
+
+    /**
+     * @return void
+     */
+    public function testWrite(): void
+    {
+        $this->expectOutputString('');
+        $this->adapter->write('Test message');
+    }
+
+    /**
+     * @return void
+     */
+    public function testWriteln(): void
+    {
+        $this->expectOutputString('');
+        $this->adapter->writeln('Test message');
+    }
+
+    /**
+     * @return void
+     */
+    public function testAsk(): void
+    {
+        $this->assertSame('Default answer', $this->adapter->ask('Test question', 'Default answer'));
+
+        $this->adapter->setIterationMode(false);
+        $this->assertSame('Default answer', $this->adapter->ask('Test question', 'Default answer'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testConfirm(): void
+    {
+        $this->assertTrue($this->adapter->confirm('Test question', true));
+
+        $this->adapter->setIterationMode(false);
+        $this->assertFalse($this->adapter->confirm('Test question', false));
+    }
+
+    /**
+     * @return void
+     */
+    public function testChoice(): void
+    {
+        $this->assertSame('Default', $this->adapter->choice('Test question', ['Test choice 1', 'Test choice 2'], 'Default'));
+
+        $this->adapter->setNoIteration();
+        $this->assertSame(1, $this->adapter->choice('Test question', ['Test choice 1', 'Test choice 2'], 1));
+    }
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->adapter = new ComposerInputOutputAdapter(new NullIO());
+    }
+}
---
+++ tests/SprykerSdkTest/Integrator/Dependency/Console/NullIOTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdkTest\Integrator\Dependency\Console;
+
+use PHPUnit\Framework\TestCase;
+use SprykerSdk\Integrator\Dependency\Console\NullIO;
+
+class NullIOTest extends TestCase
+{
+    /**
+     * @var \SprykerSdk\Integrator\Dependency\Console\NullIO
+     */
+    protected NullIO $command;
+
+    /**
+     * @return void
+     */
+    public function testWrite(): void
+    {
+        $this->expectOutputString('Output message');
+        $this->command->write('Output message');
+
+        $this->expectOutputString('Output messageOutput message 2<br>Output message 3<br>');
+        $this->command->write(
+            [
+                'Output message 2',
+                'Output message 3',
+            ],
+            true,
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testWriteln(): void
+    {
+        $this->expectOutputString('Output message 1<br>Output message 2<br><br>');
+        $this->command->writeln(
+            [
+                'Output message 1',
+                'Output message 2',
+            ],
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testAsk(): void
+    {
+        $this->assertSame(
+            'Default answer',
+            $this->command->ask('Test question here', 'Default answer'),
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testConfirm(): void
+    {
+        $this->assertFalse($this->command->confirm('Test question here', false));
+    }
+
+    /**
+     * @return void
+     */
+    public function testChoice(): void
+    {
+        $this->assertFalse($this->command->choice('Test question here', ['Option 1', 'Option 2'], false));
+    }
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->command = new NullIO();
+    }
+}
