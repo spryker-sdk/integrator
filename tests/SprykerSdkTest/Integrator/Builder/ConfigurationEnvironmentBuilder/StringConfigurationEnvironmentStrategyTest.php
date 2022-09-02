57f0adc5ec0271f6aa301f7b9ac6b4e55c833336
Dmytro Klyman <dmytro.klyman@spryker.com>
2022-09-02 14:11:36 +0200

    Increase code coverage

---
+++ tests/SprykerSdkTest/Integrator/Builder/ConfigurationEnvironmentBuilder/StringConfigurationEnvironmentStrategyTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdkTest\Integrator\Builder\ConfigurationEnvironmentBuilder;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use SprykerSdk\Integrator\Builder\ConfigurationEnvironmentBuilder\StringConfigurationEnvironmentStrategy;
+
+class StringConfigurationEnvironmentStrategyTest extends TestCase
+{
+    /**
+     * @var \SprykerSdk\Integrator\Builder\ConfigurationEnvironmentBuilder\StringConfigurationEnvironmentStrategy
+     */
+    protected StringConfigurationEnvironmentStrategy $strategy;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->strategy = new StringConfigurationEnvironmentStrategy();
+    }
+
+    /**
+     * @return \Generator
+     */
+    public function isApplicableDataProvider(): Generator
+    {
+        yield [false, []];
+        yield [false, 12345];
+        yield [false, 'test string'];
+        yield [false, 'test 125125-)(*&^%'];
+        yield [true, '\'test 125125-)(*&^%\''];
+        yield [true, "'test 125125-)(*&^%'"];
+    }
+
+    /**
+     * @dataProvider isApplicableDataProvider
+     *
+     * @param bool $expRes
+     * @param mixed $value
+     *
+     * @return void
+     */
+    public function testIsApplicable(bool $expRes, $value): void
+    {
+        $this->assertSame($expRes, $this->strategy->isApplicable($value));
+    }
+
+    /**
+     * @return \Generator
+     */
+    public function getFormattedExpressionDataProvider(): Generator
+    {
+        yield ['\'       lorem ipsum\'', "       lorem ipsum'''''''''''''''"];
+        yield ['\'lorem ipsum\'', 'lorem ipsum'];
+    }
+
+    /**
+     * @dataProvider getFormattedExpressionDataProvider
+     *
+     * @param string $expRes
+     * @param string $value
+     *
+     * @return void
+     */
+    public function testGetFormattedExpression(string $expRes, string $value): void
+    {
+        $this->assertSame($expRes, $this->strategy->getFormattedExpression($value));
+    }
+}
