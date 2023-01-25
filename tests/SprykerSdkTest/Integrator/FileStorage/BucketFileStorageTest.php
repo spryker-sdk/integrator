0e340b428f5510d40c901fc13f84afd06f78df3f
v.strelchenko <v.strelchenko@gamingtec.com>
2023-01-25 17:41:36 +0200

    SDK-1757: Task 2. Connect S3 buckets

--- tests/SprykerSdkTest/Integrator/FileStorage/BucketFileStorageTest.php
+++ tests/SprykerSdkTest/Integrator/FileStorage/BucketFileStorageTest.php
@@ -47,4 +47,61 @@ class BucketFileStorageTest extends BaseTestCase
         // Act
         $fileStorage->getFile('some-path.txt');
     }
+
+    /**
+     * @return void
+     */
+    public function testFailOnInvalidCredentialsKey(): void
+    {
+        // Assert
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Environment variable "INTEGRATOR_FILE_BUCKET_CREDENTIALS_KEY" is not set.');
+
+        // Arrange
+        $configMock = $this->createMock(IntegratorConfig::class);
+        $configMock->method('getFileBucketName')->willReturn('string');
+        $fileStorage = new BucketFileStorage($configMock);
+
+        // Act
+        $fileStorage->getFile('some-path.txt');
+    }
+
+    /**
+     * @return void
+     */
+    public function testFailOnInvalidCredentialsSecret(): void
+    {
+        // Assert
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Environment variable "INTEGRATOR_FILE_BUCKET_CREDENTIALS_SECRET" is not set.');
+
+        // Arrange
+        $configMock = $this->createMock(IntegratorConfig::class);
+        $configMock->method('getFileBucketName')->willReturn('string');
+        $configMock->method('getFileBucketCredentialsKey')->willReturn('string');
+        $fileStorage = new BucketFileStorage($configMock);
+
+        // Act
+        $fileStorage->getFile('some-path.txt');
+    }
+
+    /**
+     * @return void
+     */
+    public function testFailOnInvalidBucketRegion(): void
+    {
+        // Assert
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Environment variable "INTEGRATOR_FILE_BUCKET_REGION" is not set.');
+
+        // Arrange
+        $configMock = $this->createMock(IntegratorConfig::class);
+        $configMock->method('getFileBucketName')->willReturn('string');
+        $configMock->method('getFileBucketCredentialsKey')->willReturn('string');
+        $configMock->method('getFileBucketCredentialsSecret')->willReturn('string');
+        $fileStorage = new BucketFileStorage($configMock);
+
+        // Act
+        $fileStorage->getFile('some-path.txt');
+    }
 }
