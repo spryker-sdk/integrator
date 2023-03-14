6afd8ca56a1196a4599221b612aae34d78407e43
Sergey Romankov <sergey.romankov@spryker.com>
2023-03-14 14:19:39 +0300

    SDK-2013 add test

---
+++ tests/SprykerSdkTest/Integrator/ConfigReader/ConfigReaderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace SprykerSdkTest\Integrator\ConfigReader;
+
+use PhpParser\ParserFactory;
+use SprykerSdk\Integrator\ConfigReader\ConfigReader;
+use SprykerSdkTest\Integrator\BaseTestCase;
+
+class ConfigReaderTest extends BaseTestCase
+{
+    /**
+     * @return void
+     */
+    public function testReadShouldReturnParsedValues(): void
+    {
+        // Arrange
+        $configFilePath = __DIR__ . '/../../../_data/project_config/config_default.php';
+        $configReader = new ConfigReader(new ParserFactory());
+        $configKeys = ['KernelConstants::PROJECT_NAMESPACES', 'KernelConstants::CORE_NAMESPACES'];
+
+        // Act
+        $result = $configReader->read($configFilePath, $configKeys);
+
+        // Assert
+        $this->assertSame([
+            'KernelConstants::PROJECT_NAMESPACES' => ['Pyz'],
+            'KernelConstants::CORE_NAMESPACES' => [
+                'SprykerShop',
+                'SprykerEco',
+                'Spryker',
+                'SprykerSdk',
+            ],
+        ], $result);
+    }
+}
